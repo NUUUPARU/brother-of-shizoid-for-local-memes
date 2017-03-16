3c7669a4a728db013c5487c2a0ae853f4c9a6223
NUUUPARU <nuuuparu@rambler.ru>
2017-03-16 23:43:05 +0300

    Delete word.rb

--- shizoid-master/app/models/word.rb
+++
@@ -1,9 +0,0 @@
-class Word < ActiveRecord::Base
-  has_many :chains
-
-  def self.learn(words)
-    new_words = words - Word.where(word: words).pluck(:word)
-    Word.create(new_words.uniq.map { |word| { word: word } })
-  end
-
-end
